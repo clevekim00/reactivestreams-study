777bc83a03a1864f2ce3af249747d7a5e45d21a9
younghwan-kim <younghwan.kim@linecorp.com>
2020-10-28 10:56:07 +0900

    Project init.

---
+++ src/test/java/com/clevekim/study/reactivestreams/ReactivestreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevekim.study.reactivestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivestreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
